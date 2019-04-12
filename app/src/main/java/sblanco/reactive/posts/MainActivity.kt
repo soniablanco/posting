1804189731fe91f089c765c06837dec9d93c6cd3
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-04-12 15:52:55 +0100

    Extending button to make its clicks observable

--- app/src/main/java/sblanco/reactive/posts/MainActivity.kt
+++ app/src/main/java/sblanco/reactive/posts/MainActivity.kt
@@ -2,11 +2,19 @@ package sblanco.reactive.posts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import sblanco.reactive.posts.common.toObservable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        refresh_btn.toObservable()
+            .subscribe{
+                Log.d("Click","it worked")
+            }
     }
 }
---
+++ app/src/main/java/sblanco/reactive/posts/common/Extensions.kt
@@ -0,0 +1,15 @@
+package sblanco.reactive.posts.common
+
+import android.view.View
+import android.widget.Button
+import io.reactivex.Observable
+
+fun Button.toObservable():Observable<View>{
+    val root= this
+    return Observable.create<View>{
+            o ->
+        root.setOnClickListener {
+            o.onNext(it)
+        }
+    }
+}
